59bf16225ee4efb1c55d3bf44a10a0593ec4894e
Zane Muir <zanemuir1995@gmail.com>
2018-05-27 16:32:39 +0800

    update

--- Stimulus/7selectivity/selectivity_player.py
+++ Stimulus/7selectivity/selectivity_player.py
@@ -24,12 +24,12 @@ logging.basicConfig(level=logging.INFO)
 # low_time = 5   # low time in seconds
 
 basic_color = {
-    'black':(-1, -1, -1),
-    'white':(1, 1, 1),
-    'gray':(0, 0, 0),
-    'red':(1,0,0),
-    'green':(0,1,0),
-    'blue':(0,0,1)
+    'black': (-1, -1, -1),
+    'white': (1, 1, 1),
+    'gray': (0, 0, 0),
+    'red': (1, 0, 0),
+    'green': (0, 1, 0),
+    'blue': (0, 0, 1)
 }
 
 
